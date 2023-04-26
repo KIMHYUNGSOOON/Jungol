62f16eba3147a118835dcaf28b835a4b2f3558f9
Class01 <Class01@DESKTOP-T15JNCT>
2023-04-26 14:46:09 +0900

    509번 문제

---
+++ Jungol/src/q509/Main.java
@@ -0,0 +1,12 @@
+package q509;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = -120,b=100,c=3330000,d=84213;
+		x = -100;
+		System.out.println(x);
+	}
+
+}
--- Jungol/src/q9003/Main.java
+++ Jungol/src/q9003/Main.java
@@ -1,13 +1,9 @@
-package q9003;
+ package q9003;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("세 번째 프로그램입니다.\r\n"
-				+ "줄을 바꾸어 출력합니다.\r\n"
-				+ "여기까지 출력하고\r\n"
-				+ "줄을 바꿉니다.");
+		System.out.println("세 번째 프로그램입니다.\n줄을 바꾸어 출력합니다. \n여기까지 출력하고 \n줄을 바꿉니다.");
 	}
-
 }
