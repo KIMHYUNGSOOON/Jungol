179aaf505f20122e7c78f981f9c560cc61658d36
Class01 <Class01@DESKTOP-T15JNCT>
2023-04-27 09:45:38 +0900

    q503번문제

--- Jungol/src/q508/Main.java
+++ Jungol/src/q508/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("      item     count      price");
-		System.out.println("       pen        20        100");
-		System.out.println("      note         5         95");
-		System.out.println("    eraser       110         97");
+		System.out.println("      item     count     price");
+		System.out.println("       pen        20       100");
+		System.out.println("      note         5        95");
+		System.out.println("    eraser       110        97");
 	}
 
 }
